36ec957549bb8183a942b87184dd65dce8acb9ef
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2024-01-15 11:05:29 +0300

    gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {HashRouter} from 'react-router-dom'
 import App from './App.tsx'
 import './scss/base/base.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<HashRouter>
-		<React.StrictMode>
-			<App />
-		</React.StrictMode>
-	</HashRouter>,
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import {defineConfig} from 'vite'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/Versale/',
-  plugins: [react()],
+export default defineConfig(({command}) => {
+	const config = {
+		plugins: [react()],
+		base: '/',
+	}
+
+	if (command !== 'serve') {
+		config.base = '/Versale/'
+	}
+
+	return config
 })
