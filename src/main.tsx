1c4d8351d2d2fb0d41457fd5d8d6762698503390
Magomedsoltan Kasumov <77916046+kenzWeb@users.noreply.github.com>
2024-01-15 10:29:57 +0300

    gh-pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import {HashRouter} from 'react-router-dom'
 import App from './App.tsx'
 import './scss/base/base.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>,
+	<HashRouter>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</HashRouter>,
 )
